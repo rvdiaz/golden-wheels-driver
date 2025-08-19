9e1fccb5e7d12bf4b71edba275b3f4cd5961de62
rvdiaz <valdesdiazroberto@gmail.com>
2025-08-19 16:03:11 -0400

    adding mls history

--- src/custom_modules/tools/sections/owner_property_details/results/index.tsx
+++ src/custom_modules/tools/sections/owner_property_details/results/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { Property } from './property';
 import { IProperty } from '../interfaces';
 import { Owner } from './owner';
+import { MlsHistoryList } from './mlsHistory';
 
 export const PropertyOwnerResults = ({ propertyData }: { propertyData: IProperty }) => {
   return (
     <>
       <Property propertyData={propertyData} />
       <Owner ownerInfo={propertyData.ownerInfo} />
+      {propertyData?.mlsHistory?.length > 0 && (
+        <MlsHistoryList mlsHistory={propertyData.mlsHistory} />
+      )}
     </>
   );
 };

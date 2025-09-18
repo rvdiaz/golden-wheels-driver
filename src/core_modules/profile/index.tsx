70f4ac281119496cd547772a10847485128982a9
rvdiaz <valdesdiazroberto@gmail.com>
2025-09-18 09:29:50 -0400

    Update index.tsx

--- src/core_modules/profile/index.tsx
+++ src/core_modules/profile/index.tsx
@@ -71,7 +71,7 @@ export const ProfileScreen: React.FC = () => {
               navigation.navigate(ModuleKeys.goals as never);
             }}
             style={styles.menuItem}>
-            <Icons.DollarSign size={20} color="#374151" />
+            <Icons.TargetIcon size={20} color="#374151" />
             <Text style={styles.menuText}>Goals</Text>
             <Icons.ChevronRight size={20} color="#9CA3AF" />
           </TouchableOpacity>

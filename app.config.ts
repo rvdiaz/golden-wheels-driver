94d47de5e44441de1b31ab1d56072f5d771c3dac
rvdiaz <valdesdiazroberto@gmail.com>
2026-04-01 00:55:45 -0400

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -10,8 +10,8 @@ const BUNDLE_IDENTIFIER = 'com.codidge.goldenwheels';
 const PACKAGE_NAME = 'com.codidge.goldenwheels';
 const ICON = './assets/logo.jpeg';
 const ADAPTIVE_ICON = './assets/logo.jpeg';
-const VERSION = '1.0.7';
-const BUILD_NUMBER = 7;
+const VERSION = '1.0.8';
+const BUILD_NUMBER = 8;
 const MERCHANTID = 'merchant.com.codidge.goldenwheels';
 
 export default (arg: ConfigContext): ExpoConfig => {

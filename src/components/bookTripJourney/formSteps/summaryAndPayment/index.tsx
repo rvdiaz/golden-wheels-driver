2d05e4816976364bfde8dc38e54523eaf12e118f
rvdiaz <valdesdiazroberto@gmail.com>
2026-05-05 16:45:47 -0400

    Update index.tsx

--- src/components/bookTripJourney/formSteps/summaryAndPayment/index.tsx
+++ src/components/bookTripJourney/formSteps/summaryAndPayment/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { View, ScrollView, StyleSheet, Alert } from 'react-native';
 import { useFormContext } from 'react-hook-form';
 import {
